72d9495f265df1e075b5cd160f62649c35dfbf31
Bruce Tran <bruce.tran@gmail.com>
2020-03-27 18:32:49 -0400

    Add Cache only. It doesm't work as well but can be used.

--- public/sw.js
+++ public/sw.js
@@ -45,31 +45,38 @@ self.addEventListener('activate', function (event) {
     return self.clients.claim();
 });
 
+//self.addEventListener('fetch', function (event) {
+//    // console.log('[Service Worker] Fetching something ....', event);
+//    event.respondWith(
+//        caches.match(event.request)
+//            .then(function (response) {
+//                if (response) {
+//                    return response;
+//                } else {
+//                    return fetch(event.request)
+//                        .then(function (res) {
+//                            return caches.open(CACHE_DYNAMIC_NAME)
+//                                .then(function (cache) {
+//                                    cache.put(event.request.url, res.clone());
+//                                    return res;
+//                                })
+//                                .catch(function(err) {
+//                                    return caches.open(CACHE_STATIC_NAME)
+//                                        .then(function (cache) {
+//                                            return cache.match('/offline.html');
+//                                        });
+//                                });
+//                        });
+//                }
+//            }) 
+//    );
+//});
+
+// Cache only
 self.addEventListener('fetch', function (event) {
-    // console.log('[Service Worker] Fetching something ....', event);
     event.respondWith(
         caches.match(event.request)
-            .then(function (response) {
-                if (response) {
-                    return response;
-                } else {
-                    return fetch(event.request)
-                        .then(function (res) {
-                            return caches.open(CACHE_DYNAMIC_NAME)
-                                .then(function (cache) {
-                                    cache.put(event.request.url, res.clone());
-                                    return res;
-                                })
-                                .catch(function(err) {
-                                    return caches.open(CACHE_STATIC_NAME)
-                                        .then(function (cache) {
-                                            return cache.match('/offline.html');
-                                        });
-                                });
-                        });
-                }
-            }) 
-    );
+    );        
 });
 
  
