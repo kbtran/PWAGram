618ba732684da0d3b1cd59ea4e21ec1d4ffb459a
Bruce Tran <bruce.tran@gmail.com>
2020-03-27 18:18:40 -0400

    Add offline fallback page.

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-var CACHE_STATIC_NAME = 'static-v3';
-var CACHE_DYNAMIC_NAME = 'dynamic';
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
@@ -10,6 +10,7 @@ self.addEventListener('install', function (event) {
                 cache.addAll([
                     '/',
                     '/index.html',
+                    '/offline.html',
                     '/src/js/app.js',
                     '/src/js/feed.js',
                     '/src/js/promise.js',
@@ -60,7 +61,10 @@ self.addEventListener('fetch', function (event) {
                                     return res;
                                 })
                                 .catch(function(err) {
-
+                                    return caches.open(CACHE_STATIC_NAME)
+                                        .then(function (cache) {
+                                            return cache.match('/offline.html');
+                                        });
                                 });
                         });
                 }
