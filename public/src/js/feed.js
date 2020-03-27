667bd8e112862b0498c9638898dbf28d0c243b8a
Bruce Tran <bruce.tran@gmail.com>
2020-03-27 17:37:20 -0400

    Comment out cache on demand for now with dynamic still turned on. Usually to enable cache on demand like this, dynamic caching is disable in the fetch addEventListener (cache.pu())

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -30,17 +30,18 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
-function onSaveButtonClicked(event) {
-    console.log('clicked');
-    // check if browser supports caching
-    if ('caches' in window) {
-        caches.open('user-requested')
-            .then(function (cache) {
-                cache.add('https://httpbin.org/get');
-                cache.add('/src/images/sf-boat.jpg');
-            });
-    }
-}
+// Currently not in use, allows to save assets in cache on demand otherwise
+//function onSaveButtonClicked(event) {
+//    console.log('clicked');
+//    // check if browser supports caching
+//    if ('caches' in window) {
+//        caches.open('user-requested')
+//            .then(function (cache) {
+//                cache.add('https://httpbin.org/get');
+//                cache.add('/src/images/sf-boat.jpg');
+//            });
+//    }
+//}
 
 function createCard() {
     var cardWrapper = document.createElement('div');
@@ -60,10 +61,10 @@ function createCard() {
     cardSupportingText.className = 'mdl-card__supporting-text';
     cardSupportingText.textContent = 'In San Francisco';
     cardSupportingText.style.textAlign = 'center';
-    var cardSaveButton = document.createElement('button');
-    cardSaveButton.textContent = 'Save';
-    cardSaveButton.addEventListener('click', onSaveButtonClicked);
-    cardSupportingText.appendChild(cardSaveButton);
+    //var cardSaveButton = document.createElement('button');
+    //cardSaveButton.textContent = 'Save';
+    //cardSaveButton.addEventListener('click', onSaveButtonClicked);
+    //cardSupportingText.appendChild(cardSaveButton);
     cardWrapper.appendChild(cardSupportingText);
     componentHandler.upgradeElement(cardWrapper);
     sharedMomentsArea.appendChild(cardWrapper);
