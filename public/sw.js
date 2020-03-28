b88da2407a3f668ca0207ce184154dc8841847a1
Bruce Tran <bruce.tran@gmail.com>
2020-03-28 17:36:38 -0400

    Comment out trimCache()

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_STATIC_NAME = 'static-v14';
+var CACHE_STATIC_NAME = 'static-v15';
 var CACHE_DYNAMIC_NAME = 'dynamic-v3';
 var STATIC_FILES = [
     '/',
@@ -17,18 +17,18 @@ var STATIC_FILES = [
     'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
- function trimCache(cacheName, maxItems) {
-   caches.open(cacheName)
-     .then(function (cache) {
-       return cache.keys()
-         .then(function (keys) {
-           if (keys.length > maxItems) {
-             cache.delete(keys[0])
-               .then(trimCache(cacheName, maxItems));
-           }
-         });
-     })
- }
+ //function trimCache(cacheName, maxItems) {
+ //  caches.open(cacheName)
+ //    .then(function (cache) {
+ //      return cache.keys()
+ //        .then(function (keys) {
+ //          if (keys.length > maxItems) {
+ //            cache.delete(keys[0])
+ //              .then(trimCache(cacheName, maxItems));
+ //          }
+ //        });
+ //    })
+ //}
 
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
@@ -91,7 +91,7 @@ self.addEventListener('fetch', function (event) {
                 .then(function (cache) {
                     return fetch(event.request)
                         .then(function (res) {
-                            trimCache(CACHE_DYNAMIC_NAME, 3);
+                            // trimCache(CACHE_DYNAMIC_NAME, 3);
                             cache.put(event.request, res.clone());
                             return res;
                         });
@@ -112,7 +112,7 @@ self.addEventListener('fetch', function (event) {
                             .then(function (res) {
                                 return caches.open(CACHE_DYNAMIC_NAME)
                                     .then(function (cache) {
-                                        trimCache(CACHE_DYNAMIC_NAME, 3);
+                                       // trimCache(CACHE_DYNAMIC_NAME, 3);
                                         cache.put(event.request.url, res.clone());
                                         return res;
                                     })
