565a0cafc827e22509c2eef8282f8d57ad8457c5
Bruce Tran <bruce.tran@gmail.com>
2020-03-27 21:51:15 -0400

    Add cache then network with offline support

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 var CACHE_STATIC_NAME = 'static-v7';
-var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var CACHE_DYNAMIC_NAME = 'dynamic-v3';
 
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
@@ -45,18 +45,46 @@ self.addEventListener('activate', function (event) {
     return self.clients.claim();
 });
 
-// Cache then network and dynamic caching
+// Cache then network with offline support
 self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.open(CACHE_DYNAMIC_NAME)
-            .then(function (cache) {
-                return fetch(event.request)
-                    .then(function (res) {
-                        cache.put(event.request, res.clone());
-                        return res;
-                    });
-            })
-    );
+    var url = 'https://httpbin.org/get';
+
+    if (event.request.url.indexOf(url) > -1) {
+        event.respondWith(
+            caches.open(CACHE_DYNAMIC_NAME)
+                .then(function (cache) {
+                    return fetch(event.request)
+                        .then(function (res) {
+                            cache.put(event.request, res.clone());
+                            return res;
+                        });
+                })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then(function (response) {
+                    if (response) {
+                        return response;
+                    } else {
+                        return fetch(event.request)
+                            .then(function (res) {
+                                return caches.open(CACHE_DYNAMIC_NAME)
+                                    .then(function (cache) {
+                                        cache.put(event.request.url, res.clone());
+                                        return res;
+                                    })
+                                    .catch(function (err) {
+                                        return caches.open(CACHE_STATIC_NAME)
+                                            .then(function (cache) {
+                                                return cache.match('/offline.html');
+                                            });
+                                    });
+                            });
+                    }
+                })
+        );      
+    }
 });
 
 //self.addEventListener('fetch', function (event) {
