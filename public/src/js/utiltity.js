91e76bf66c9cd1cf192cc7dbada9f05fc893db23
Bruce Tran <bruce.tran@gmail.com>
2020-03-28 21:12:25 -0400

    Add utiltity.js to handle create IndexDB and write to it. Update sw.js to use it.

---
+++ public/src/js/utiltity.js
@@ -0,0 +1,17 @@
+// Create Index DB posts-store if it doesn't exist.
+var dbPromise = idb.open('posts-store', 1, function (db) {
+  if (!db.objectStoreNames.contains('posts')) {
+    db.createObjectStore('posts', {keyPath: 'id'});
+  }
+});
+
+// Write to IndexDB
+function writeData(st, data) {
+  return dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readwrite');
+      var store = tx.objectStore(st);
+      store.put(data);
+      return tx.complete;
+    });
+}
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
 importScripts('/src/js/idb.js');
+importScripts('/src/js/utility.js');
 
 var CACHE_STATIC_NAME = 'static-v16';
 var CACHE_DYNAMIC_NAME = 'dynamic-v3';
@@ -20,12 +21,6 @@ var STATIC_FILES = [
     'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
-// Create Index DB posts-store if it doesn't exist.
-var dbPromise = idb.open('posts-store', 1, function (db) {
-    if (!db.objectStoreNames.contains('posts')) {
-        db.createObjectStore('posts', { keyPath: 'id' });
-    }
-});
 
  //function trimCache(cacheName, maxItems) {
  //  caches.open(cacheName)
@@ -102,13 +97,7 @@ self.addEventListener('fetch', function (event) {
                 clonedRes.json()
                     .then(function (data) {
                         for (var key in data) {
-                            dbPromise
-                                .then(function (db) {
-                                    var tx = db.transaction('posts', 'readwrite');
-                                    var store = tx.objectStore('posts');
-                                    store.put(data[key]);
-                                    return tx.complete;
-                                });
+                            writeData('posts', data[key]);
                         }
                     });
                 return res;
@@ -147,64 +136,6 @@ self.addEventListener('fetch', function (event) {
         );      
     }
 });
-
-// Cache then network with dynamic caching strategy 
-//self.addEventListener('fetch', function (event) {
-//     console.log('[Service Worker] Fetching something ....', event);
-//    event.respondWith(
-//        caches.match(event.request)
-//            .then(function (response) {
-//                if (response) {
-//                    return response;
-//                } else {
-//                    return fetch(event.request)
-//                        .then(function (res) {
-//                            return caches.open(CACHE_DYNAMIC_NAME)
-//                                .then(function (cache) {
-//                                    cache.put(event.request.url, res.clone());
-//                                    return res;
-//                                })
-//                                .catch(function (err) {
-//                                    return caches.open(CACHE_STATIC_NAME)
-//                                        .then(function (cache) {
-//                                            return cache.match('/offline.html');
-//                                        });
-//                                });
-//                        });
-//                }
-//            })
-//    );
-//});
-
-// Network with cache fallback
-//self.addEventListener('fetch', function (event) {
-//    event.respondWith(
-//        fetch(event.request)
-//            .then(function (res) {
-//                return caches.open(CACHE_DYNAMIC_NAME)
-//                    .then(function (cache) {
-//                        cache.put(event.request.url, res.clone());
-//                        return res;
-//                    });
-//            })
-//            .catch(function (err) {
-//                return caches.match(event.request);
-//            })
-//    );
-//});
-
-// Cache only
-//self.addEventListener('fetch', function (event) {
-//    event.respondWith(
-//        caches.match(event.request)
-//    );        
-//});
-
-// Network only with Service worker
-//self.addEventListener('fetch', function (event) {
-//    event.respondWith(
-//        fetch(event.request)
-//    );
-//});
+ 
 
  
