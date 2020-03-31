20e302b676af211df0ec5e6104a635d853528495
Bruce Tran <bruce.tran@gmail.com>
2020-03-31 18:17:43 -0400

    Add actions to notifications.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -34,7 +34,11 @@ function displayConfirmNotification() {
             vibrate: [100, 50, 200],
             badge: '/src/images/icons/app-icon-96x96.png',
             tag: 'confirm-notification',
-            renotify: true
+            renotify: true,
+            actions: [
+                { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
+                { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
+            ]
         };
 
         navigator.serviceWorker.ready
