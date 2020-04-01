f43ec6ae327a5924a266668e077ea5227bfcfbd6
Bruce Tran <bruce.tran@gmail.com>
2020-03-31 21:36:19 -0400

    Add push notification logic, add web-push and store subscription to firebase database.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -43,9 +43,53 @@ function displayConfirmNotification() {
 
         navigator.serviceWorker.ready
             .then(function (swreg) {
-                swreg.showNotification('Successfully subscribed (from SW)!', options);
+                swreg.showNotification('Successfully subscribed!', options);
             });
     }
+}
+
+function configurePushSub() {
+    if (!('serviceWorker' in navigator)) {
+        return;
+    }
+
+    var reg;
+    navigator.serviceWorker.ready
+        .then(function (swreg) {
+            reg = swreg;
+            return swreg.pushManager.getSubscription();
+        })
+        .then(function (sub) {
+            if (sub === null) {
+                // Create a new subscription
+                var vapidPublicKey = 'BIny2JQpyOuFowfTb6pT1ag6hp3BJRoQ1agNWLR3NZU2D7NJeO2xuBiSDePB580ojUkBuPfOBZCFfxtzlyO6e9Q';
+                var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+                return reg.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: convertedVapidPublicKey
+                });
+            } else {
+                // We have a subscription
+            }
+        })
+        .then(function (newSub) {
+            return fetch('https://pwagram-6478c.firebaseio.com/subscriptions.json', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(newSub)
+            });
+        })
+        .then(function (res) {
+            if (res.ok) {
+                displayConfirmNotification();
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
 function askForNotificationPermission() {
@@ -54,11 +98,13 @@ function askForNotificationPermission() {
         if (result !== 'granted') {
             console.log('No notification permission granted!');
         } else {
-            displayConfirmNotification();
+            configurePushSub();
+            // displayConfirmNotification();
         }
     });
 }
-if ('Notification' in window) {
+
+if ('Notification' in window && 'serviceWorker' in navigator) {
     // Enable all buttons
     for (var i = 0; i < enableNotificationsButtons.length; i++) {
         enableNotificationsButtons[i].style.display = 'inline-block';
