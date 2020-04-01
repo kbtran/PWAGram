30b7c199c9560761b2e1f9011b3a93e6d77ee75d
Bruce Tran <bruce.tran@gmail.com>
2020-03-31 22:55:46 -0400

    Cleanup

--- functions/index.js
+++ functions/index.js
@@ -5,36 +5,12 @@ const admin = require('firebase-admin');
 const cors = require('cors')({ origin: true });
 const webpush = require('web-push');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-//exports.helloWorld = functions.https.onRequest((request, response) => {
-//    cors(request, response, () => { });
-//    return response.send("Hello from Firebase!");
-//});
-
 var serviceAccount = require("./pwagram-fb-key.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://pwagram-6478c.firebaseio.com/'
 });
-
-//exports.storePostData = functions.https.onRequest((request, response) => {
-//    return cors(request, response, () => {
-//        admin.database().ref('posts').push({
-//            id: request.body.id,
-//            title: request.body.title,
-//            location: request.body.location,
-//            image: request.body.image
-//        })
-//    });
-//});
-
 
 exports.storePostData = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
