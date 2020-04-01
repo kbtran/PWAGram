51440c02fd13da7ab06a249b8549267c4e8b9d80
Bruce Tran <bruce.tran@gmail.com>
2020-03-31 22:50:47 -0400

    Update storePostData()

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 const cors = require('cors')({ origin: true });
+const webpush = require('web-push');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -24,13 +24,30 @@ admin.initializeApp({
     databaseURL: 'https://pwagram-6478c.firebaseio.com/'
 });
 
+//exports.storePostData = functions.https.onRequest((request, response) => {
+//    return cors(request, response, () => {
+//        admin.database().ref('posts').push({
+//            id: request.body.id,
+//            title: request.body.title,
+//            location: request.body.location,
+//            image: request.body.image
+//        })
+//    });
+//});
+
+
 exports.storePostData = functions.https.onRequest((request, response) => {
-    return cors(request, response, () => {
+    cors(request, response, () => {
         admin.database().ref('posts').push({
             id: request.body.id,
             title: request.body.title,
             location: request.body.location,
             image: request.body.image
-        })
+        }).then(() => {
+            return response.status(201).json({ message: 'Data stored', id: request.body.id });
+        }).catch((err) => {
+            return response.status(500).json({ error: err });
+        });
     });
 });
+
